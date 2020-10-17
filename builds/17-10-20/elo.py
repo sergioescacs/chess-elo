6a416039137d011986a1efaccea385cf7bc78a07
4sergio44 <38814235+4sergio44@users.noreply.github.com>
2020-10-17 21:11:46 +0200

    beta 0.2.1.

--- builds/17-10-20/elo.py
+++
@@ -1,5 +0,0 @@
-def calcular(k, elo1, elo2, result): #main function   
-    ea = 1/(1+10**((int(elo2)-int(elo1))/400)) #theorically result, using probability and other mathmatics methods. 
-    new_elo = round(int(elo1)+(k*(int(result)-ea))) #final variation calculation the theorically result, the real result and the k constant.
-    
-    return new_elo #final ELO points
