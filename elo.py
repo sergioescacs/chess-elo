5e382e5fa5f6cfeecf32db72a4f3783e8b8e8244
4sergio44 <38814235+4sergio44@users.noreply.github.com>
2020-10-17 21:06:04 +0200

    beta 0.2.1.

--- elo.py
+++ elo.py
@@ -1,5 +1,5 @@
 def calcular(k, elo1, elo2, result): #main function   
-    ea = 1/(1+10**((int(elo2)-int(elo1))/400)) #theorically result, using probability and other mathmatics methods. 
-    new_elo = round(int(elo1)+(k*(int(result)-ea))) #final variation calculation the theorically result, the real result and the k constant.
+    ea = 1/(1+10**((elo2-elo1)/400)) #theorically result, using probability and other mathmatics methods. 
+    new_elo = round(elo1+(k*(result-ea)), 1) #final variation calculation the theorically result, the real result and the k constant.
     
     return new_elo #final ELO points
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from tkinter import messagebox
 
 root = Tk()
 
-photo = PhotoImage(file = "xadrez.png")
+photo = PhotoImage(file = "xadrez.png") #set the future icon photo as a variable
+
+#process the image and set it as the main icon of the app
 root.iconphoto(False, photo)
 root.title("ELO calc.")
 root.resizable(False, False)
@@ -18,34 +20,49 @@ variable_r = StringVar(root)
 variable_r.set(results[0])
 
 #Deploy Labels and various content as buttons and option lists
-main = LabelFrame(root, text="Chess ELO app - beta 0.2.0.")
-txt1 = Label(main, text = "Your ELO: ")
+main = LabelFrame(root, text="Chess ELO app - beta 0.2.1.", font=('Arial', 10, "bold"))
+txt1 = Label(main, text = "Your ELO: ", font=('Helvetica', 9))
 E1 = Entry(main, bd =3)
-txt2 = Label(main, text = "Your enemy's ELO: ")
+txt2 = Label(main, text = "Your enemy's ELO: ", font=('Helvetica', 9))
 E2 = Entry(main, bd =3)
+txt3 = Label(main, text = "K: ", font=('Helvetica', 9))
 
 #K option list
 mb = OptionMenu(main, variable, *opt )
-mb.config(width=20, font=('Helvetica', 12))
+mb.config(width=8, font=('Helvetica', 9))
 
+txt4 = Label(main, text = "Result: ", font=('Helvetica', 9))
 #Result option list
 mb_r = OptionMenu(main, variable_r, *results )
-mb_r.config(width=20, font=('Helvetica', 12))
+mb_r.config(width=8, font=('Helvetica', 9))
 
 def submit_info(): #first grab all information and prepare the call to the elo.py algorithm
-    messagebox.showinfo("ELO variation", E1.get()+"  -->  "+ str(elo.calcular(int(variable.get()), int(E1.get()), int(E2.get()), int(variable_r.get()))))
+    try: #try method to avoid any issue
+        x = elo.calcular(int(variable.get()), int(E1.get()), int(E2.get()), float(variable_r.get()))
+    
+    except: #message in case of issue during the process
+        messagebox.showerror("Warning!", "Invalid entry, take your time and read carefully the instructions")
+    
+    #conditionals to make the info better for user, they filter between positive and negative variations
+    if x > int(E1.get()):
+        messagebox.showinfo("ELO variation", E1.get()+" + "+ str(round(x-int(E1.get()), 1)) + " = "+ str(x))
+        
+    else:
+        messagebox.showinfo("ELO variation", E1.get()+" - "+ str(round(int(E1.get())-x, 1)) + " = "+ str(x))
 
-Submit = Button(main, text ="Submit", command = submit_info) #submit button
+Submit = Button(main, text ="CALC", command = submit_info) #submit button
 
 #pack content & show on screen
-main.pack(fill="both", expand="yes")
-txt1.pack()
-E1.pack()
-txt2.pack()
-E2.pack()
-mb.pack()
-mb_r.pack()
+main.grid()
+txt1.grid(row = 0, column = 0, sticky = W)
+E1.grid(row = 0, column = 1, sticky = W)
+txt2.grid(row = 1, column = 0, sticky = W)
+E2.grid(row = 1, column = 1, sticky = W)
+txt3.grid(row = 2, column = 0, sticky = W)
+mb.grid(row = 2, column = 1, sticky = N)
+txt4.grid(row = 3, column = 0, sticky = W)
+mb_r.grid(row = 3, column = 1, sticky = N)
 
-Submit.pack(side = RIGHT)
+Submit.grid(row = 4, column = 1, sticky = E)
  
-root.mainloop()
+root.mainloop() #tkinter main loop
