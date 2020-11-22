d03107a01f942961094972a94c2c9bf1448bde36
4sergio44 <38814235+4sergio44@users.noreply.github.com>
2020-11-22 16:45:37 +0100

    beta v0.3.1

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-# Chess ELO app - Beta 0.3.0.
+# Chess ELO app - Beta v0.3.0.
+![](https://img.shields.io/github/languages/top/sergioescacs/chess-elo)
+![](https://img.shields.io/github/repo-size/sergioescacs/chess-elo?color=critical)
+![](https://img.shields.io/github/license/sergioescacs/chess-elo?color=success)
+![](https://img.shields.io/github/v/release/sergioescacs/chess-elo?color=yellow&include_prereleases)
+![](https://img.shields.io/github/stars/sergioescacs/chess-elo?style=social)
 
-As a chess player one thing  wanna know is how will change my ELO rating after winning or loseing any match?
-Well, thanks to my long experience playing chess, i've been playing since 2010, i can realize something: internet has shown us hundreads of chess tools but noone of them are as useful as u want. 
+As a chess player one thing I wanna know is how my ELO will change after a competition.
+Well, thanks to my long experience playing chess, i've been playing since 2010, i can realize something: internet has shown us hundreads of chess tools but none of them are as useful as we want. 
 
-As a result of that iv'e been working of that project. I'll develop a new chess ELO calculator with new features and useful tools. If u want to know more things about it, please suscribe to GitHub repository and be ready to contribute ;) 
+As a result: that new project with big goals on it. I'll be developing a new chess ELO calculator with new features and useful tools. If u want to know more things about it, please suscribe to my GitHub repository and be ready to contribute ;) 
 
 # Q & A
-1. How can i contribute?
-You can easily download all the content by using the GitHub clone repository mode. Make your changes and request a pull :)
+<b> 1. How can i contribute? </b>
+<p>You can easily download all the content by using the GitHub clone repository mode. Make your changes and pull a request :)</p>
 
-3. Is it going to be available for android or IOS?
-Yes it is. We are working on the windows version, but in the future we will create both mobile versions. 
+<b>3. Is it going to be available for android or IOS?</b>
+<p>Yes it is. I am still working on the windows version, but in the future we will create both mobile ports. </p>
 
-2. How can i get the application?
-Visit the "builds" section and download the last build, execute the .exe file and ejnoy!
+<b>2. How can i get the application?</b>
+<p>Visit the "builds" section and download the last build, execute the .exe file and ejnoy! Or, download the last installer from: https://sergioescacs.itch.io/chess-elo-calculator</p>
 
-    -
--- main.py
+++ main.py
@@ -2,156 +2,161 @@ import elo
 from tkinter import *
 from tkinter import messagebox
 
-root = Tk()
-
-photo = PhotoImage(file = "xadrez.png") #set the future icon photo as a variable
-
-#process the image and set it as the main icon of the app
-root.iconphoto(False, photo)
-root.title("ELO calc.")
-root.resizable(False, False)
-
-opt = ["k10", "k13", "k16", "k20", "k23", "k24", "k30", "k32", "k40"] #k constant option list
-variable = StringVar(root)
-variable.set(opt[0])
-
-results = ["1", "0.5", "0"]#result option list
-variable_r = StringVar(root)
-variable_r.set(results[0])
-
-#add Box variables 
-all_entries = []
-all_txt = []
-all_mb = []
-counter = 0
-
-reference = {}
-
-def process_dict(d):
-    e = 0.0
+class Constructor: #Main builder class where extra-buttons and labels will be created
+    def __init__(self, master, main, k_list, results, counter):
+        #Various variables from the App class
+        self.reference = {}
+        self.master = master
+        self.main = main
+        self.k_list = k_list
+        self.results = results
+        self.counter = counter
         
-    for x in d:
-        e += float((d.get(x)).get())
-    
-    return e
-    
-def addEntry(): #addBox function
-    global counter #counter variable
-    global all_entries #all_entries array variable
-    global all_txt
-    global all_mb
-    global reference
-    
-    if counter < 9: #first of all, check if we have not created more than 10 boxes
+        self.reference[self.counter] = StringVar(self.master) #create a section from dictionary with StringVar caract
+        self.reference[self.counter].set(self.results[0]) #set default value
         
-        txt_n = Label(main, text = "{}. ".format(str(counter+2)), font=('Helvetica', 9)) #crate txt object
-        txt_n.grid(row = 4+counter, column = 0, sticky = W, padx = (5, 0) ) #show in screen txt object
+        self.txt_n = Label(self.main, text = "{}. ".format(str(self.counter+2)), font=('Helvetica', 9)) #crate txt object
+        self.txt_n.grid(row = 4+counter, column = 0, sticky = W, padx = (5, 0) ) #show in screen txt object
         
-        ent = Entry(main, bd = 3, width = 15) #create entry pbject
-        ent.grid(row = 4+counter, column = 0, sticky = W, padx = (25, 0)) #show in screen entry object
+        self.ent = Entry(self.main, bd = 3, width = 15) #create entry pbject
+        self.ent.grid(row = 4+self.counter, column = 0, sticky = W, padx = (25, 0)) #show in screen entry object
         
-        reference[counter] = StringVar(root) #create a section from dictionary with StringVar caract
-        reference[counter].set(results[0]) #set default value
+        self.r = OptionMenu(self.main, self.reference[self.counter], *self.results) #create optionMenu and set all variables
+        self.r.config(width=4, font=('Helvetica', 9)) #Config optionMenu
+        self.r.grid(row = 4+self.counter, column = 1, sticky = W, padx =(20, 20)) #show in screen new optionMenu    
         
-        r = OptionMenu(main, reference[counter], *results) #create optionMenu and set all variables
-        r.config(width=4, font=('Helvetica', 9)) #Config optionMenu
-        
-        r.grid(row = 4+counter, column = 1, sticky = W, padx =(20, 20)) #show in screen new optionMenu
-
-        all_entries.append(ent) #save entry into an array
-        all_txt.append(txt_n) #save txt name into an array
-        all_mb.append(r) #save optionmenu name into array
-        
-        counter += 1
-
-def deleteEntry():
-    global all_entries
-    global all_txt
-    global all_mb
-    global counter
-    global reference
-    
-    try:
-        #modify all array 
-        all_entries[len(all_entries)-1].destroy()
-        del all_entries[len(all_entries)-1]
+    def get_info(self): #Function where all data will be processed
+        e = 0.0
+                
+        for x in self.reference:
+            e += float((self.reference.get(x)).get())
             
-        all_txt[len(all_txt)-1].destroy()
-        del all_txt[len(all_txt)-1]
+        return self.ent.get(), e
+    
+    def __del__(self):
+        self.txt_n.destroy() #Destroy number[x] label
+        self.ent.destroy() #Destroy entry[x] 
+        self.r.destroy() #Destroy k-box[x]
+
+    
+class App: #Main class where the functions will be implemented.      
+    def __init__(self, master):
+        #Main variables
+        global counter
+        global pre_fab
+        pre_fab, counter = [], 0
+        
+        k_list = ["k10", "k13", "k16", "k20", "k23", "k24", "k30", "k32", "k40"] #k constant option list
+        variable = StringVar(master)
+        variable.set(k_list[0])
+
+        results = ["1", "0.5", "0"]#result option list
+        variable_r = StringVar(master)
+        variable_r.set(results[0])
+        
+        # Generate Main Frame
+        main = LabelFrame(master, text="Chess ELO app - beta v0.3.1", font=('Arial', 10, "bold"))
+        
+        #Main sub-functions
+        def submit_info(): #submit button on-Click
+            punct = 0
+            data_ea = []
             
-        all_mb[len(all_mb)-1].destroy()
-        del reference[int(len(all_mb)-1)]
-        del all_mb[len(all_mb)-1]
-        print(process_dict(reference))
+            for x in pre_fab:
+                punct += x.get_info()[1]
+                data_ea.append(int(x.get_info()[0]))
+
+            data_r = float(punct) + float(variable_r.get())
+            data_ea.append(int(E2.get()))
+
+            try: #try method to avoid any issue
+                ea = elo.t_result(data_ea, int(E1.get()))
+                y = elo.calcular(int((variable.get()).replace("k", "")), int(E1.get()), data_r, ea)
+                
+            except Exception as e: #message in case of issue during the process
+                print(e)
+                messagebox.showerror("Warning!", "Invalid entry, take your time and read carefully the instructions")
+            
+            #conditionals to make the info better for user, they filter between positive and negative variations
+            if y > int(E1.get()):
+                messagebox.showinfo("ELO variation", "{} +  {}  = {}".format(E1.get(), str(round(y-int(E1.get()), 1)), str(y)))
+                
+            else:
+                messagebox.showinfo("ELO variation", "{} - {} = {}".format(E1.get(), str(round(int(E1.get())-y, 1)), str(y)))
+
+            data_r = 0.0
+                    
+        def addEntry(): #on-Click function, add Entry button. 
+            global counter
+            global pre_fab
+            
+            if counter < 9:
+                pre_fab.append(Constructor(master, main, k_list, results, counter))
+                counter += 1
+             
+            else:
+                messagebox.showerror("Warning!", "You can not add more Labels.")
         
+        def deleteEntry(): #on-Click function, delete Entry button. 
+            global counter
+            global pre_fab
+            
+            try:
+                del pre_fab[counter-1] 
+                counter -= 1
+            
+            except:
+                messagebox.showerror("Warning!", "No entries to delete!")
+            
+        #Start building widgets
+        txt1 = Label(main, text = "Your ELO: ", font=('Helvetica', 9))
+        E1 = Entry(main, bd =3, width = 15)
+        txt2 = Label(main, text = "Your enemy's ELO: ", font=('Helvetica', 9))
+        E2 = Entry(main, bd =3, width = 15)
+        txt3 = Label(main, text = "1. ", font=('Helvetica', 9))
+
+            #K option list
+        mb = OptionMenu(main, variable, *k_list )
+        mb.config(width=4, font=('Helvetica', 9))
+
+            #Result option list
+        mb_r = OptionMenu(main, variable_r, *results )
+        mb_r.config(width=4, font=('Helvetica', 9))
+
+        Submit = Button(main, text ="CALC", command = submit_info) #submit button
+        Add = Button(main, text ="ADD", command = addEntry) #add entry button
+        Del = Button(main, text = "DELETE", command = deleteEntry) #add Delete entry button
         
-        counter -= 1
-    
-    except:
-        messagebox.showerror("Warning!", "No entries to delete!")
-    
-    #print(all_entries)
+        #Final screen deploy
+        main.grid()
+        txt1.grid(row = 0, column = 0, sticky = W, padx = (10, 0))
+        main.grid()
+        txt1.grid(row = 0, column = 0, sticky = W, padx = (10, 0))
+        Submit.grid(row = 0, column = 0, sticky = E)
+        Del.grid(row = 0, column = 1, sticky = E, padx = (55, 10))
+        Add.grid(row = 0, column = 1, sticky = W, padx = 15)
 
-def submit_info(): #first grab all information and prepare the call to the elo.py algorithm
-    
-    data_r = process_dict(reference) + float(variable_r.get())
-    data_ea = []
-    
-    for x in all_entries:
-        data_ea.append(int(x.get()))
-    
-    data_ea.append(int(E2.get()))
-    
-    try: #try method to avoid any issue
-        ea = elo.t_result(data_ea, int(E1.get()))
-        x = elo.calcular(int((variable.get()).replace("k", "")), int(E1.get()), int(E2.get()), data_r, ea)
-        
-    except: #message in case of issue during the process
-        messagebox.showerror("Warning!", "Invalid entry, take your time and read carefully the instructions")
-    
-    #conditionals to make the info better for user, they filter between positive and negative variations
-    if x > int(E1.get()):
-        messagebox.showinfo("ELO variation", "{} +  {}  = {}".format(E1.get(), str(round(x-int(E1.get()), 1)), str(x)))
-        
-    else:
-        messagebox.showinfo("ELO variation", "{} - {} = {}".format(E1.get(), str(round(int(E1.get())-x, 1)), str(x)))
+        E1.grid(row = 1, column = 0, sticky = W, padx = (25, 0))
+        mb.grid(row = 1, column = 1, sticky = W, padx =(20, 20))
+        txt2.grid(row = 2, column = 0, sticky = W, padx = (10, 0))
+        txt3.grid(row = 3, column = 0, sticky = W, padx = (5, 0) )
+        E2.grid(row = 3, column = 0, sticky = W, padx = (25, 0))
+        mb_r.grid(row = 3, column = 1, sticky = W, padx =(20, 20))
 
-    data_r = 0.0
-    
-    
-#Deploy Labels and various content as buttons and option lists
-main = LabelFrame(root, text="Chess ELO app - beta v0.3.0", font=('Arial', 10, "bold"))
-txt1 = Label(main, text = "Your ELO: ", font=('Helvetica', 9))
-E1 = Entry(main, bd =3, width = 15)
-txt2 = Label(main, text = "Your enemy's ELO: ", font=('Helvetica', 9))
-E2 = Entry(main, bd =3, width = 15)
-txt3 = Label(main, text = "1. ", font=('Helvetica', 9))
+#Main function where all actions are coordinated 
+def main():
+    root = Tk() #Init of the main loop
 
-#K option list
-mb = OptionMenu(main, variable, *opt )
-mb.config(width=4, font=('Helvetica', 9))
+    photo = PhotoImage(file = "xadrez.png") #set ico as a variable
 
-#Result option list
-mb_r = OptionMenu(main, variable_r, *results )
-mb_r.config(width=4, font=('Helvetica', 9))
+    root.iconphoto(False, photo)
+    root.title("ELO calc.") #Set window title
+    root.resizable(False, False) #window none resizable
 
-Submit = Button(main, text ="CALC", command = submit_info) #submit button
-Add = Button(main, text ="ADD", command = addEntry) #add entry button
-Del = Button(main, text = "DELETE", command = deleteEntry) #add Delete entry button
+    Window_Base = App(root) #Deploying of the main items
 
-#pack content & show on screen
-main.grid()
-txt1.grid(row = 0, column = 0, sticky = W, padx = (10, 0))
-Submit.grid(row = 0, column = 0, sticky = E)
-Del.grid(row = 0, column = 1, sticky = E, padx = (55, 10))
-Add.grid(row = 0, column = 1, sticky = W, padx = 15)
+    root.mainloop()
 
-E1.grid(row = 1, column = 0, sticky = W, padx = (25, 0))
-mb.grid(row = 1, column = 1, sticky = W, padx =(20, 20))
-txt2.grid(row = 2, column = 0, sticky = W, padx = (10, 0))
-txt3.grid(row = 3, column = 0, sticky = W, padx = (5, 0) )
-E2.grid(row = 3, column = 0, sticky = W, padx = (25, 0))
-mb_r.grid(row = 3, column = 1, sticky = W, padx =(20, 20))
-
-
-root.mainloop() #tkinter main loop
+#Where the software starts
+if __name__ == '__main__':
+    main()
